4ab32199799c2357d568bf3ceccb891ead86229c
Ricardo Nunes <ricardonascimentosp138@gmail.com>
2022-12-29 11:42:48 -0300

    Atualização mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
     UsersModule,
     MongooseModule.forRoot(
-      'mongodb+srv://ricardo:spMaOTnX3WL8lZfb@cluster0.csbmfut.mongodb.net/test',
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.csbmfut.mongodb.net/test`,
     ),
   ],
   controllers: [],
